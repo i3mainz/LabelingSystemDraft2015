a27e773f9e64186ade933689b7a8f445566c61ff
Florian Thiery <web@florian-thiery.de>
2015-05-13 09:58:01 +0200

    Update impressum.php

--- client/Strawberry/labelingworkbench/impressum.php
+++ client/Strawberry/labelingworkbench/impressum.php
@@ -12,7 +12,7 @@ require_once("models/headline.php");
 echo "	
 <div id='content'>
 	<br>
-	<h1>Impressum</h1>
+	<h1>About</h1>
 	<h2>The Labeling System</h2>
 	<br>
 	<div id='left-nav'>";
@@ -23,42 +23,40 @@ echo "
 </div>
 
 <div id='maingrey'>
-<br><br>
+<p>&nbsp;</p>
+
 <center>
-<h1>The Partner</h1>
-<br><br>
+<h1>Partner</h1>
+<br>
 <a href='http://www.ieg-mainz.de' target='_blank'><img src='models/site-templates/images/ieg.gif' height='200'></a>
 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
 <a href='http://www.i3mainz.hs-mainz.de' target='_blank'><img src='models/site-templates/images/i3mainz-logo.png' height='200'></a>
+<p>&nbsp;</p>
+<p><b>
+Florian Thiery (Developer, Idea and Concept) [i3mainz] </br>
+Prof. Dr. phil. Kai Christian Bruhn (Idea and Concept) [i3mainz] </br>
+Dr. Michael Piotrowski (Idea and Concept) [IEG]</br>
+Giovanni Colavizza (Idea and Concept) [IEG]</br>
+</b>
+</p>
 </center>
-<br>
-<br>
-<h1>Labels</h1>
+<p>&nbsp;</p>
+
 <center>
+<h1>Project</h1>
+<p>Find out more about the project at the <a href='http://i3mainz.hs-mainz.de/de/projekte/labelingsystem' target='_blank'>i3mainz website</a>.</p>
+</center>
+<p>&nbsp;</p>
+
+<center>
+<h1>Licences</h1>
 <p>The created Labels are licenced by <a href='https://creativecommons.org/licenses/by-sa/4.0/' target='_blank'>CC BY SA 4.0</a>&nbsp;<img src='models/site-templates/images/ccbysa.png'></p>
+<p>The Labeling System is developed by i3mainz - Institute for Spatial Information and Surveying Technology under a <a href='https://github.com/i3mainz/LabelingSystem/blob/master/LICENSE' target='_blank'>MIT License</a>.</p>
+<p>The User Cake Management System is developed by Adam Davis and Jonathan Cassels under a <a href='http://usercake.com/licence.txt' target='_blank'>MIT License</a>.</p>
+<p>The sourcecode is available on <a href='https://github.com/i3mainz/LabelingSystem' target='_blank'>GitHub</a>.</p>
 </center>
-<p></p>
-<br>
-<h1>userCake Managementsystem</h1>
-<center>
-<p>Copyright (c) 2009-2012</p>
-<p>Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the 'Software'), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:</p>
-<p>The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.</p>
-<p>THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.</p>
-</center>
-<br><br><br>
+<p>&nbsp;</p>
+
 </div>";
 
 require_once("models/footerline.php");
